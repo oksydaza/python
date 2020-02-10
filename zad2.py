f8fea6682d2b76474e7444aec6af9e97ad33a461
oksydaza <magdawlazlo@gmail.com>
2020-02-10 20:47:16 +0100

    Add get_test() and main() functions

--- zad2.py
+++ zad2.py
@@ -1,6 +1,15 @@
 TEXTFILE = "input.txt"
 
-with open(TEXTFILE, 'r') as f:
-    data = f.read()
+def get_text(input_file):
+    with open(input_file, 'r') as f:
+        data = f.read()
 
-print(data)
+    return(data)
+
+def main():
+    text = get_text(TEXTFILE)
+    print(text)
+
+if __name__== "__main__":
+  main()
+
